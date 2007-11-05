1acab38aea1e2056d4e5add2a18c01c036441c78
plindenbaum <plindenbaum@386288ff-973c-0410-9a7f-93f66fbed5da>
2007-11-05 22:08:06 +0000

    continue

--- trunk/src/java/org/lindenb/swing/SimpleDialog.java
+++ trunk/src/java/org/lindenb/swing/SimpleDialog.java
@@ -4,17 +4,23 @@
 package org.lindenb.swing;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 
@@ -29,7 +35,7 @@ public class SimpleDialog extends JDialog
 	public static final int OK_OPTION= JOptionPane.OK_OPTION;
 	public static final int CANCEL_OPTION= JOptionPane.CANCEL_OPTION;
 	private ConstrainedAction<SimpleDialog> okAction;
-
+	private JTextField errorMessageLabel;
 	protected JPanel contentPane;
 	protected JPanel bottomPane;
 	private int status=CANCEL_OPTION;
@@ -43,15 +49,43 @@ public class SimpleDialog extends JDialog
 		this.bottomPane= new JPanel(new FlowLayout(FlowLayout.TRAILING));
 		this.contentPane.add(this.bottomPane,BorderLayout.SOUTH);
 		JButton button1;
+		
+		this.errorMessageLabel= new JTextField("",20);
+		this.errorMessageLabel.setForeground(Color.RED);
+		this.errorMessageLabel.setEditable(false);
+		this.bottomPane.add(this.errorMessageLabel);
+		this.errorMessageLabel.setBorder(new EmptyBorder(0,0,0,0));
+		this.errorMessageLabel.setFont(new Font("Dialog",Font.PLAIN,9));
+		this.bottomPane.add(new JSeparator(JSeparator.VERTICAL));
+		
 		this.bottomPane.add(button1=new JButton(this.okAction= new ConstrainedAction<SimpleDialog>(this,"OK")
 			{
 			private static final long serialVersionUID = 1L;
-
+			
+			@Override
+			public String getErrorMessage() {
+				String s= super.getErrorMessage();
+				SimpleDialog.this.setErrorMessage(s);
+				return s;
+				}
+			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				closeDialogWithStatus(OK_OPTION);
 				}
 			}));
+		
+		this.okAction.addPropertyChangeListener(new PropertyChangeListener()
+			{
+			@Override
+			public void propertyChange(PropertyChangeEvent evt)
+				{
+				if("enabled".equals(evt.getPropertyName()))
+					{
+					setErrorMessage(okAction.getErrorMessage());
+					}
+				}
+			});
 		
 		JButton button2;
 		this.bottomPane.add(button2=new JButton(new AbstractAction("Cancel")
@@ -69,6 +103,13 @@ public class SimpleDialog extends JDialog
 		button2.setPreferredSize(d2);
 		}
 
+	protected void setErrorMessage(String s)
+		{
+		this.errorMessageLabel.setText(s==null?"":s);
+		this.errorMessageLabel.setToolTipText(s);
+		this.errorMessageLabel.setCaretPosition(0);
+		}
+	
 	protected void closeDialogWithStatus(int status)
 		{
 		this.status= status;
