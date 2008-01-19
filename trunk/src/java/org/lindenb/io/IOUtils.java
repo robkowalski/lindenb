66f72f569dcfdce458e6236e1d49d971494788ba
plindenbaum <plindenbaum@386288ff-973c-0410-9a7f-93f66fbed5da>
2008-01-19 22:21:42 +0000

    cont

--- trunk/src/java/org/lindenb/io/IOUtils.java
+++ trunk/src/java/org/lindenb/io/IOUtils.java
@@ -9,12 +9,22 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
+import java.io.StringWriter;
 import java.io.Writer;
 import java.net.URL;
 import java.util.zip.GZIPInputStream;
 
 public class IOUtils
 	{
+	public static String getFileContent(File file) throws IOException
+		{
+		StringWriter w= new StringWriter();
+		FileReader r=new FileReader(file);
+		copyTo(r,w);
+		r.close();
+		return w.toString();
+		}
+	
 	public static void copyTo(InputStream in, OutputStream out) throws IOException
 		{
 		byte buffer[]=new byte[2048];
@@ -58,16 +68,19 @@ public class IOUtils
 	    		}
 	    	else
 	        	{
-	        	File fin= new File(uri);
-	        	if(fin.getName().endsWith(".gz"))
-	        		{
-	        		return new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(fin))));
-	        		}
-	        	else
-	        		{
-	        		return new BufferedReader(new FileReader(fin));
-	        		}
+	        	return openFile( new File(uri));
 	        	}
 		}
+	
+	/** open a file and return a BufferedReader, gunzip the file if it ends with *.gz*/
+	public static BufferedReader openFile(File file) throws IOException
+		{
+		if(	file.getName().endsWith(".gz"))
+			{
+			return new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(file))));
+			}
+		return new BufferedReader(new FileReader(file));
+		}
 
+	
 }
---
+++ trunk/src/java/org/lindenb/lang/ResourceUtils.java
@@ -0,0 +1,76 @@
+package org.lindenb.lang;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+import org.lindenb.io.IOUtils;
+
+public class ResourceUtils {
+protected ResourceUtils() {}
+
+public static URL getResource(Class<?> clazz,String name) throws IOException
+	{
+	if(clazz==null) throw new NullPointerException("class is null");
+	if(name==null) throw new NullPointerException("name is null");
+	URL url= clazz.getResource(name);
+	if(url==null) throw new IOException(clazz.toString()+" cannot getResource("+name+")");
+	return url;
+	}
+
+public static InputStream getResourceAsStream(Class<?> clazz,String name) throws IOException
+	{
+	if(clazz==null) throw new NullPointerException("class is null");
+	if(name==null) throw new NullPointerException("name is null");
+	InputStream in= clazz.getResourceAsStream(name);
+	if(in==null) throw new IOException(clazz.toString()+" cannot getResourceAsStream("+name+")");
+	return in;
+	}
+
+public static String getContent(Class<?> clazz,String name) throws IOException
+	{
+	StringWriter sw= new StringWriter();
+	InputStreamReader r= new InputStreamReader(getResourceAsStream(clazz, name));
+	IOUtils.copyTo(r, sw);
+	r.close();
+	return sw.toString();
+	}
+
+public static String getContent(Class<?> clazz,String name,String defaultContent)
+	{
+	try {
+		return getContent(clazz, name);
+		}
+	catch (IOException e)
+		{
+		return defaultContent;
+		}
+	}
+
+public static BufferedReader openReader(Class<?> clazz,String name) throws IOException
+	{
+	return new  BufferedReader(new InputStreamReader(getResourceAsStream(clazz, name)));
+	}
+
+public static Icon getIcon(Class<?> clazz,String name) throws IOException
+	{
+	return new ImageIcon(getResource(clazz, name));
+	}
+
+public static Icon getIcon(Class<?> clazz,String name,Icon defaultIcon)
+	{
+	try {
+		return getIcon(clazz, name);
+		}
+	catch (IOException e)
+		{
+		return defaultIcon;
+		}
+	}
+}
