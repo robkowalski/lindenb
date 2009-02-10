df8bf941fb071d95151e5e7a66485c748d3b9ba4
plindenbaum <plindenbaum@386288ff-973c-0410-9a7f-93f66fbed5da>
2009-02-10 21:16:16 +0000

    cont

--- proj/tinyxul/translate/defaults/preferences/prefs.js
+++ proj/tinyxul/translate/defaults/preferences/prefs.js
@@ -1,1 +1,8 @@
 pref("toolkit.defaultChromeURI", "chrome://translate/content/translate.xul");
+
+/* debugging prefs */
+pref("browser.dom.window.dump.enabled", true);
+pref("javascript.options.showInConsole", true);
+pref("javascript.options.strict", true);
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
