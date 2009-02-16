79cc5b3f779e94313189929a6590384faa9cb1e2
plindenbaum <plindenbaum@386288ff-973c-0410-9a7f-93f66fbed5da>
2009-02-16 22:04:12 +0000

    cont

--- trunk/proj/tinyxul/xuldb/chrome/xuldb/library.js
+++ trunk/proj/tinyxul/xuldb/chrome/xuldb/library.js
@@ -1,27 +1,35 @@
 /**
  * IsA 
  */
-function IsA()
-        {
-        }
+var IsA={
+	URI:function(uri)
+		{
+		try
+			{
+			var  nsURI = Components
+			.classes["@mozilla.org/network/simple-uri;1"]
+			.getService(Components.interfaces.nsIURI);
+			nsURI.spec = uri;
+			return true;
+			}
+		catch(err2)
+			{
+			return false;
+			}
+		}
+	};
 
-IsA.URI=function(uri)
-        {
-        try
-                {
-                var  nsURI = Components
-                .classes["@mozilla.org/network/simple-uri;1"]
-                .getService(Components.interfaces.nsIURI);
-                nsURI.spec = uri;
-                return true;
-                }
-        catch(err2)
-                {
-                return false;
-                }
-        }
+var XUL={NS:"http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul"};
+var HTML={NS:"http://www.w3.org/1999/xhtml"};
+var RDF={NS:"http://www.w3.org/1999/02/22-rdf-syntax-ns#"};
+var RDFS={NS:"http://www.w3.org/2000/01/rdf-schema#"};
+var XLINK={NS:"http://www.w3.org/1999/xlink"};
+var SVG={NS:"http://www.w3.org/2000/svg"};
+var DC={NS:"http://purl.org/dc/elements/1.1/"};
 
-
-
-
- 
+function $(id)
+	{
+	dump("Searching for "+id);
+	var e = document.getElementById(id);
+	return e;
+	}
---
+++ trunk/proj/tinyxul/xuldb/chrome/xuldb/xuldb.js
@@ -0,0 +1,94 @@
+
+
+
+
+
+function XULDB()
+	{
+	var file = Components.classes["@mozilla.org/file/directory_service;1"]
+                        .getService(Components.interfaces.nsIProperties)
+                        .get("ProfD", Components.interfaces.nsIFile);
+
+	file.append("xuldb.sqlite");
+
+	var storageService = Components.classes["@mozilla.org/storage/service;1"]
+		.getService(Components.interfaces.mozIStorageService);
+	//see https://developer.mozilla.org/en/mozIStorageConnection
+	this.connection = storageService.openDatabase(file);
+	
+	if(!this.connection.tableExists("version"))
+		{
+		this.connection.createTable("version","version INTEGER PRIMARY KEY AUTOINCREMENT");
+		this.connection.executeSimpleSQL("insert or ignore into version(version) values (1)");
+		}
+	
+	if(!this.connection.tableExists("RDFClass"))
+		{
+		this.connection.createTable("RDFClass",
+			"id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+			"label TEXT NOT NULL,"+
+			"description TEXT NOT NULL,"+
+			"uri TEXT UNIQUE NOT NULL,"+
+			"parent INTEGER NOT NULL REFERENCES RDFClass(id)"
+			);
+		this.connection.executeSimpleSQL("insert or ignore into RDFClass(id,label,description,uri,parent) values (1,\"rdfs:Class\",\"rdfs:Class\",\""+ RDFS.NS +"Class\",1)");
+		}
+	
+	}
+
+	
+
+var env={
+	xul:null,
+	window:null
+	};
+
+function buildTree(root, id)
+	{
+	var stmt=env.xul.connection.createStatement("select label from RDFClass where id=?1");
+	stmt.bindInt32Parameter(0,id);
+ 	if(!stmt.executeStep()) return;
+	 	
+	var tchildren = document.createElementNS(XUL.NS,"treechildren");
+	var titem = document.createElementNS(XUL.NS,"treeitem");
+	var trow = document.createElementNS(XUL.NS,"treerow");
+	var tcell = document.createElementNS(XUL.NS,"treecell");
+	
+	tchildren.appendChild(titem);
+	titem.appendChild(trow);
+	trow.appendChild(tcell);
+	titem.setAttribute("container","true");
+	titem.setAttribute("open","true");
+	tcell.setAttribute("label",stmt.getString(0));
+	root.appendChild(tchildren);
+	
+	/** loop over the children of id */
+	var array= new Array();
+	stmt=env.xul.connection.createStatement("select id from RDFClass where parent=?1 and id!=1 order by label");
+	stmt.bindInt32Parameter(0,id);
+	while(stmt.executeStep())
+		{
+		array.push(stmt.getInt32(0));
+		}
+	for(var i=0;i < array.length;++i)
+		{
+		/** recursive call */
+		buildTree(titem,array[i]);
+		}
+	};
+
+function documentLoaded()
+	{
+	try
+		{
+		env.xul= new XULDB();
+		env.window= window;
+		buildTree($("tree-class"),1);
+		}
+	catch(e)
+		{
+		alert(e.message);
+		}
+	};
+
+	
