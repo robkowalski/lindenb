c0f4e42e21b2eb076a9034f8a78c37ca8a6a3e63
plindenbaum <plindenbaum@386288ff-973c-0410-9a7f-93f66fbed5da>
2009-06-06 12:55:31 +0000

    cont

---
+++ proj/mwrdf/src/java/org/lindenb/mwrdf/MWRdfEditor.java
@@ -0,0 +1,684 @@
+package org.lindenb.mwrdf;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.net.URL;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import java.util.List;
+
+
+import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.text.PlainDocument;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.multipart.MultipartRequestEntity;
+import org.apache.commons.httpclient.methods.multipart.Part;
+import org.apache.commons.httpclient.methods.multipart.StringPart;
+import org.lindenb.lang.InvalidXMLException;
+import org.lindenb.lang.ThrowablePane;
+import org.lindenb.me.Me;
+import org.lindenb.sw.dom.DOM4RDF;
+import org.lindenb.sw.vocabulary.RDF;
+import org.lindenb.swing.DocumentAdapter;
+import org.lindenb.util.C;
+import org.lindenb.util.Digest;
+import org.lindenb.xml.XMLUtilities;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.Attributes;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * 
+ * MWRdfEditor
+ *
+ */
+public class MWRdfEditor extends JApplet
+	{
+	private static final long serialVersionUID = 1L;
+	protected static final String ACTION_PREVIEW="action.article.preview";
+	protected static final String ACTION_POST="action.article.post";
+	protected static final String ACTION_ABOUT_ME="action.about.me";
+	protected static final String ACTION_INFO_SYS="action.info.sys";
+	private ActionMap actionMap= new ActionMap();
+	private JTextField infoBoxField;
+	private JTextArea textArea;
+	private DocumentBuilder docBuilder;
+	private SAXParser saxParser;
+	
+	public static final String PARAM_USERNAME="userName";
+	public static final String PARAM_USERID="userId";
+	public static final String PARAM_SESSION="_session";
+	public static final String PARAM_WIKIPREFIX="cookiePrefix";
+	public static final String PARAM_TITLE="title";
+	
+	private static final String START_TAGS="<div id=\'rdf-content\'><pre><nowiki>";
+	private static final String END_TAGS="</nowiki></pre></div>";
+	
+	
+	/** httpClient */
+	private HttpClient httpClient=null;
+	/** RDF parse */
+	private DOM4RDF rdfParser= new DOM4RDF();
+	
+	
+    private static class GetRevisionHandler
+    extends DefaultHandler
+	    {
+	    private StringBuilder sb=null;
+	    private String content=null;
+	    GetRevisionHandler()
+	            {
+	            }
+	
+	    @Override
+	    public void startElement(String uri, String localName, String name,
+	                    Attributes attributes) throws SAXException
+	            {
+	            if(this.content==null && name.equals("rev"))
+	                    {
+	                    this.sb= new StringBuilder();
+	                    }
+	            }
+	
+	    @Override
+	    public void endElement(String uri, String localName, String name)
+	                    throws SAXException
+	            {
+	            if(sb!=null)
+	                    {
+	                    this.content=sb.toString();
+	                    this.sb=null;
+	                    }
+	            }
+	
+	    public String getContent()
+	            {
+	            return this.content;
+	            }
+	
+	    @Override
+	    public void characters(char[] ch, int start, int length)
+	                    throws SAXException {
+	            if(sb!=null)
+	            	{
+	            	sb.append(ch,start,length);
+	            	}
+	            }
+	
+	    }
+
+	
+	
+	/** constructor */
+	public MWRdfEditor()
+		{
+		
+		}
+	
+	/** anwser the http client */
+	public HttpClient getHttpClient() {
+		return httpClient;
+		}
+	
+	/** get cookiePrefix */
+	protected String getWikiPrefix()
+		{
+		return getNonNullParameter(PARAM_WIKIPREFIX);
+		}
+	
+	/** anwser _session */ 
+	protected String getSession()
+		{
+		return getNonNullParameter(PARAM_SESSION);
+		}
+	
+
+	
+	/** answer user name */
+	protected String getUserName()
+		{
+		return getNonNullParameter(PARAM_USERNAME);
+		}
+	
+	protected String getParameter(String key,String defaultValue)
+		{
+		String s= getParameter(key);
+		return s==null?defaultValue:s;
+		}
+	
+	protected String getNonNullParameter(String key)
+		{
+		return getParameter(key,"");
+		}
+	
+	
+	protected ActionMap getActionMap() {
+		return actionMap;
+		}
+	
+	/** answer user ID */
+	protected String getUserId()
+		{
+		return getNonNullParameter(PARAM_USERID);
+		}
+	
+	/** answer page title */
+	protected String getPageTitle()
+		{
+		return getNonNullParameter(PARAM_TITLE);
+		}
+	
+	
+	//http://localhost/mediawiki-1.13.3/api.php?action=query&pageids=1&prop=revisions&rvprop=content
+	private String getMediaWikiApiUrl()
+		{
+		String path=getDocumentBase().toExternalForm();
+		int i=path.lastIndexOf('?');
+		if(i!=-1) path=path.substring(0,i);
+		i=path.lastIndexOf('/');
+		if(i==-1) return path;
+		return path.substring(0,i)+"/api.php";
+		}
+	
+    public String getRevisionContent() throws IOException
+	    {
+	    
+	    NameValuePair params[]=new NameValuePair[]
+                 {
+	    		 new NameValuePair("action","query"),
+	    		 new NameValuePair("format","xml"),
+	    		 new NameValuePair("revids",getNonNullParameter("revId")),
+	    		 new NameValuePair("prop","revisions"),
+	    		 new NameValuePair("rvprop","content|ids")
+                 };
+	   
+	    
+	    GetMethod method=new GetMethod(getMediaWikiApiUrl());
+	    method.setQueryString(params);
+
+	   
+	    
+	    InputStream in=null;
+	    try 	{
+	            GetRevisionHandler h= new GetRevisionHandler();
+	            getHttpClient().executeMethod(method);
+	            in= method.getResponseBodyAsStream();
+	            this.saxParser.parse(in, h);
+	            if(h.getContent()==null)
+	                    {
+	                    return "<rdf:RDF/>";
+	                    }
+	            return h.getContent();
+	            }
+	    catch (IOException e1)
+        	{
+	    	throw e1;
+        	}
+	    catch (SAXException e1)
+            {
+            throw new IOException(e1);
+            }
+	    finally
+    		{
+	    	if(in!=null) in.close();
+	    	if(method!=null) method.releaseConnection();
+    		}
+	    }
+
+	
+	@Override
+	public void init()
+		{
+		try
+			{
+			this.httpClient= new HttpClient();
+			this.httpClient.getHttpConnectionManager().getParams().setConnectionTimeout(10000);
+			
+			
+			SAXParserFactory saxParserFactory= SAXParserFactory.newInstance();
+            saxParserFactory.setNamespaceAware(false);
+            saxParserFactory.setValidating(false);
+            this.saxParser= saxParserFactory.newSAXParser();
+            
+			DocumentBuilderFactory domFactory= DocumentBuilderFactory.newInstance();
+			domFactory.setCoalescing(true);
+			domFactory.setExpandEntityReferences(true);
+			domFactory.setIgnoringComments(true);
+			domFactory.setNamespaceAware(true);
+			domFactory.setValidating(false);
+			domFactory.setNamespaceAware(true);
+			this.docBuilder = domFactory.newDocumentBuilder();
+			this.docBuilder.setErrorHandler(new ErrorHandler()
+				{
+				@Override
+				public void error(SAXParseException exception)
+					{
+					
+					}
+				@Override
+				public void fatalError(SAXParseException err)
+							throws SAXException {
+						throw err;
+						}
+				@Override
+				public void warning(SAXParseException err)
+							throws SAXException {
+					throw err;
+						}
+				});
+			
+			
+			JMenuBar bar= new JMenuBar();
+			
+			JMenu menu= new JMenu("Applet");
+			bar.add(menu);
+			
+			
+			
+			JPanel contentPanel= new JPanel(new BorderLayout());
+			contentPanel.setBorder(
+					new CompoundBorder(new LineBorder(Color.GRAY,1),new EmptyBorder(5,5,5,5)));
+			
+				
+			
+			
+				
+				
+				
+				JPanel top= new JPanel(new FlowLayout(FlowLayout.LEADING));
+				JLabel label=new JLabel(getPageTitle());
+				label.setToolTipText(getPageTitle());
+				top.add(label);
+				contentPanel.add(top,BorderLayout.NORTH);
+				
+				
+				AbstractAction action=new AbstractAction("About me")
+					{
+					private static final long serialVersionUID = 1L;
+		
+					@Override
+					public void actionPerformed(ActionEvent e)
+						{
+						showAboutMe();
+						}
+					};
+				action.putValue(AbstractAction.SHORT_DESCRIPTION, "About me");
+				action.putValue(AbstractAction.LONG_DESCRIPTION, "About me");
+				getActionMap().put(ACTION_ABOUT_ME, action);
+				menu.add(action);
+				
+				action=new AbstractAction("System Info")
+					{
+					private static final long serialVersionUID = 1L;
+		
+					@Override
+					public void actionPerformed(ActionEvent e)
+						{
+						showSysInfo();
+						}
+					};
+				action.putValue(AbstractAction.SHORT_DESCRIPTION, "Show System Info");
+				action.putValue(AbstractAction.LONG_DESCRIPTION, "Show System Info");
+				getActionMap().put(ACTION_INFO_SYS, action);
+				menu.add(action);
+				menu.add(new JSeparator());
+				
+				JPanel bot = new JPanel(new FlowLayout(FlowLayout.LEADING));
+				contentPanel.add(bot,BorderLayout.SOUTH);
+				
+				this.infoBoxField= new JTextField(50);
+				this.infoBoxField.setEditable(false);
+				bot.add(this.infoBoxField);
+				
+				action=new AbstractAction("Post")
+					{
+					private static final long serialVersionUID = 1L;
+		
+					@Override
+					public void actionPerformed(ActionEvent e)
+						{
+						postArticle();
+						}
+					};
+				action.putValue(AbstractAction.SHORT_DESCRIPTION, "Post");
+				action.putValue(AbstractAction.LONG_DESCRIPTION, "Post");
+				getActionMap().put(ACTION_POST, action);
+				bot.add(new JButton(action));
+				action.setEnabled(false);
+				menu.add(action);
+				
+				action=new AbstractAction("Preview")
+					{
+					private static final long serialVersionUID = 1L;
+		
+					@Override
+					public void actionPerformed(ActionEvent e)
+						{
+						previewArticle();
+						}
+					};
+				action.putValue(AbstractAction.SHORT_DESCRIPTION, "Preview");
+				action.putValue(AbstractAction.LONG_DESCRIPTION, "Preview");
+				getActionMap().put(ACTION_PREVIEW, action);
+				//bot.add(new JButton(action));
+				
+			PlainDocument doc=new PlainDocument();
+			this.textArea= new JTextArea(doc);
+			this.textArea.setFont(new Font("Courier",Font.PLAIN,12));
+			doc.addDocumentListener(new DocumentAdapter()
+				{
+				@Override
+				public void documentChanged(DocumentEvent e)
+					{
+					validateRDF();
+					}
+				});	
+			
+			contentPanel.add(new JScrollPane(this.textArea),BorderLayout.CENTER);
+			if(1==2)
+				{
+				String wpTextbox1=getNonNullParameter("wpTextbox1");
+				int i= wpTextbox1.indexOf(START_TAGS);
+				if(i!=-1)
+					{
+					i+=START_TAGS.length();
+					int j= wpTextbox1.indexOf(END_TAGS,i);
+					if(j!=-1)
+						{
+						wpTextbox1= C.unescape(XMLUtilities.unescape( wpTextbox1.substring(i,j)));
+						}
+					else
+						{
+						wpTextbox1= "CANNOT GET RDF IN :\n"+wpTextbox1;
+						}
+					}
+				else
+					{
+					if(wpTextbox1.trim().length()>0)
+						{
+						wpTextbox1= "CANNOT GET RDF IN :\n"+wpTextbox1;
+						}
+					else
+						{
+						wpTextbox1=null;
+						}
+					}
+				if(wpTextbox1==null)
+					{
+					wpTextbox1="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+							"<rdf:RDF xmlns:rdf=\""+RDF.NS+"\">\n"+
+							"</rdf:RDF>";
+					}
+				this.textArea.setText(wpTextbox1);	
+				}
+			else
+				{
+				String content= getRevisionContent();
+				this.textArea.setText(content);		
+				}
+			setContentPane(contentPanel);
+			setJMenuBar(bar);
+			}
+		catch (Exception error)
+			{
+			setContentPane(new ThrowablePane(error,error.getMessage()));
+			}
+		}
+	
+	
+	private static String escapeXML(CharSequence s)
+	    {
+	    if(s==null) throw new NullPointerException("XML.escape(null)");
+	    int needed=-1;
+	    for(int i=0;i< s.length();++i)
+	        {
+	        switch(s.charAt(i))
+	            {
+	            case '\"':
+				case '&': 
+				case '<':
+				case '>':  needed=i; break;
+	
+	            default: break;
+	            }
+	        if(needed!=-1) break;
+	        }
+		if(needed==-1) return s.toString();
+		StringBuilder buffer=new StringBuilder(s.subSequence(0,needed));
+		for(int i=needed;i< s.length();++i)
+		       {
+		       switch(s.charAt(i))
+		               {
+		               case '\"': buffer.append("&quot;"); break;
+		               case '&': buffer.append("&amp;"); break;
+		               case '<': buffer.append("&lt;"); break;
+		               case '>': buffer.append("&gt;"); break;
+		               default:  buffer.append(s.charAt(i));break;
+		               }
+		       }
+		return buffer.toString();
+	    }
+	
+	public String getArticleContent() throws SAXException,IOException
+		{
+		Document dom= getDocument();
+		if(dom==null) throw new SAXException("No document");
+		
+		return  START_TAGS+
+				escapeXML(this.textArea.getText())+
+				END_TAGS+"\n[[Category:RDF doc]]";
+		
+		}
+	
+	private Document getDocument() throws SAXException,IOException
+		{
+		return this.docBuilder.parse(new InputSource(new StringReader(this.textArea.getText())));
+		}
+	
+	private void validateRDF()
+		{
+		try {
+			Document dom=getDocument();
+			Element root= dom.getDocumentElement();
+			if(root==null) throw new InvalidXMLException(dom,"No root");
+			if(!XMLUtilities.isA(root, RDF.NS, "RDF")) throw new InvalidXMLException(dom,"Not a RDF root");
+			if(XMLUtilities.count(root)!=1) throw new InvalidXMLException(root,"Expected one and only one element under rdf:RDF");	
+			this.rdfParser.parse(dom);
+			getActionMap().get(ACTION_POST).setEnabled(true);
+			this.infoBoxField.setText("OK");
+			this.infoBoxField.setForeground(Color.GREEN);
+			} 
+		catch (Exception err)
+			{
+			String msg=err.getMessage();
+			if(msg==null) msg= err.getClass().getName();
+			getActionMap().get(ACTION_POST).setEnabled(false);
+			this.infoBoxField.setText(msg);
+			this.infoBoxField.setForeground(Color.RED);
+			}
+		finally
+			{
+			this.infoBoxField.setCaretPosition(0);
+			}
+		}
+	
+	
+	public String getSummary()
+		{
+		return ""+new Date();
+		}
+	
+	public void showAboutMe()
+		{
+		JOptionPane.showMessageDialog(null, Me.FIRST_NAME+" "+Me.LAST_NAME+" "+Me.MAIL, "About me", JOptionPane.PLAIN_MESSAGE);
+		}
+	
+	public void showSysInfo()
+		{
+		StringBuilder b= new StringBuilder();
+		b.append("username "+this.getUserName()+"\n");
+		b.append("userId "+this.getUserId()+"\n");
+		b.append("_session "+getSession()+"\n");
+		b.append("wikiPrefix "+getWikiPrefix()+"\n");
+		b.append("base "+this.getCodeBase()+"\n");
+		b.append("base.path "+this.getDocumentBase().getPath()+"\n");
+		b.append("base.file "+this.getDocumentBase().getFile()+"\n");
+		b.append("base.host "+this.getDocumentBase().getHost()+"\n");
+		b.append("base.userinfo "+this.getDocumentBase().getUserInfo()+"\n");
+		b.append("base.authority "+this.getDocumentBase().getAuthority()+"\n");
+		b.append("base.protocol "+this.getDocumentBase().getProtocol()+"\n");
+		b.append("base.port "+this.getDocumentBase().getPort()+"\n");
+		b.append("base.query "+this.getDocumentBase().getQuery()+"\n");
+		b.append("base.ref "+this.getDocumentBase().getRef()+"\n");
+		b.append("docbase "+this.getDocumentBase()+"\n");
+		b.append("java "+System.getProperty("java.version","?")+"\n");
+		b.append("java.vm "+System.getProperty("java.vm.version","?")+"\n");
+		
+		for(String s:new String[]{
+				"wpSection","wpEdittime","wpScrolltop","wpStarttime","wpEditToken","wpTextbox1"})
+			{
+			b.append(s+" "+C.unescape(getNonNullParameter(s))+"\n");
+			}
+		
+		JTextArea textArea=new JTextArea(b.toString());
+		textArea.setPreferredSize(this.getSize());
+		JOptionPane.showMessageDialog(null,new JScrollPane(textArea),"System",JOptionPane.PLAIN_MESSAGE);
+		}
+	
+	
+	public void postArticle()
+		{
+		try
+			{
+			sendData("wpSave","Save page");
+			String base= getDocumentBase().toExternalForm();
+			final String editToken ="&action=edit";
+			int i= base.indexOf(editToken);
+			if(i!=-1)
+				{
+				base= base.substring(0,i)+
+					((i+editToken.length())< base.length()?base.substring(i+editToken.length()):"");
+				}
+			getAppletContext().showDocument(
+				new URL(base)
+				);
+			}
+		catch(Exception err)
+			{
+			
+			}
+		}
+	
+	public void previewArticle()
+		{
+		try
+			{
+			sendData("wpPreview","Show preview");
+			}
+		catch(Exception err)
+			{
+			ThrowablePane.show(this, err);
+			}
+		}
+	
+	protected int sendData(String submitName,String submitValue)
+		throws HttpException,IOException,SAXException
+		{
+		PostMethod postMethod=null;
+		try
+			{
+			postMethod = new PostMethod(
+					getDocumentBase().toString()
+					);
+			postMethod.getParams().setCookiePolicy(org.apache.commons.httpclient.cookie.CookiePolicy.IGNORE_COOKIES);
+			
+		
+			postMethod.addRequestHeader("Cookie",
+				getWikiPrefix()+"_session="+getSession()+"; "+
+				getWikiPrefix()+"UserID="+getUserId()+"; "+
+				getWikiPrefix()+"UserName="+getUserName()+"; "
+				);	
+	
+			List<Part> parts= new ArrayList<Part>();
+			for(String s:new String[]{
+					"wpSection","wpEdittime","wpScrolltop","wpStarttime","wpEditToken"})
+				{
+				parts.add(new StringPart(s,C.unescape(getNonNullParameter(s))));
+				}
+			parts.add(new StringPart("action","edit"));
+			parts.add(new StringPart("wpTextbox1",getArticleContent()));
+			
+			parts.add(new StringPart("wpSummary",getSummary()));
+			parts.add(new StringPart("wpAutoSummary",
+					Digest.MD5.isImplemented()? Digest.MD5.encrypt(getSummary())
+					:"")
+					);
+			parts.add(new StringPart(submitName,submitValue));
+			
+			
+			MultipartRequestEntity requestEntity= new MultipartRequestEntity(parts.toArray(new Part[parts.size()]),postMethod.getParams());
+			postMethod.setRequestEntity(requestEntity);
+			int status = getHttpClient().executeMethod(postMethod);
+			
+			/*String html= IOUtils.getReaderContent(new InputStreamReader(postMethod.getResponseBodyAsStream())).trim();
+			getContentPane().removeAll();
+			getContentPane().add(new JScrollPane(new JTextArea(html)));
+			getContentPane().validate();*/
+			return status;
+			}
+		catch(HttpException  err)
+			{
+			throw err;
+			}
+		catch(IOException err)
+			{
+			throw err;
+			}
+		finally
+			{
+			if(postMethod!=null) postMethod.releaseConnection();
+			}
+		}
+	
+	
+	
+	}
---
+++ proj/mwrdf/src/php/RDFEdit.php
@@ -0,0 +1,59 @@
+<?php
+$wgExtensionCredits['other'][] = array(
+        'name' => 'RDFEditor',
+        'version' => 0.1,
+        'author' => 'Pierre Lindenbaum PhD plindenbaum@yahoo.fr',
+        'url' => 'http://plindenbaum.blogspot.com',
+        'description' => 'java based RDF editor for MediaWiki'
+);
+
+
+global $wgHooks;
+ 
+$wgHooks['AlternateEdit'][] = 'fnMyRdfEditor';
+
+
+
+function fnMyRdfEditor(&$editpage) {
+global $wgOut, $wgRequest, $wgParser, $wgUser, $wgCookiePrefix, $wgScriptPath,$wgTitle;
+
+if(	$wgRequest->getText("java")=="false" ||
+	$wgRequest->getCheck( 'wpSave' ) ||
+    $wgRequest->getCheck( 'wpDiff' ) ||
+    $wgTitle->getNamespace() != NS_MAIN ) return true;
+
+$editpage->importFormData($wgRequest);
+$editpage->initialiseForm();
+$wgOut->addHTML("<applet name='mw_editor_applet' id='mw_editor_applet'" . 
+        " code='org/lindenb/mwrdf/MWRdfEditor.class' ".
+		" codebase='mwrdf/WEB-INF/' ".
+		" archive='commons-httpclient.jar, commons-logging.jar, commons-codec.jar, mwrdfedit.jar'" .
+		" width='800' height='500' ".
+             	" >\n");
+
+$cookieNames = array("UserName", "UserID", "_session");
+foreach($cookieNames as $ndx => $value) {
+    $cookieKey = $wgCookiePrefix . $value;
+    if (!array_key_exists($cookieKey, $_COOKIE)) continue;
+	$wgOut->addHTML("<param name='" . htmlspecialchars($value) . "' value='" . htmlspecialchars($_COOKIE[$cookieKey]) . "'/>");
+ }
+$wgOut->addHTML("<param name='cookiePrefix' value='".htmlspecialchars($wgCookiePrefix)."'/>");
+
+
+$wgOut->addHTML("<param name='wpSection' value='".htmlspecialchars($editpage->section)."'/>");
+$wgOut->addHTML("<param name='wpEdittime' value='".htmlspecialchars($editpage->edittime)."'/>");
+$wgOut->addHTML("<param name='wpScrolltop' value='".htmlspecialchars($editpage->scrolltop)."'/>");
+$wgOut->addHTML("<param name='wpStarttime' value='".htmlspecialchars($editpage->starttime)."'/>");
+$wgOut->addHTML("<param name='wpSummary' value=\"".str_replace("'","&apos;",htmlspecialchars($editpage->summary))."\"/>");
+$wgOut->addHTML("<param name='wpAutoSummary' value='".md5( $editpage->summary )."'/>");
+$wgOut->addHTML("<param name='wpEditToken' value='". $wgUser->editToken()."'/>");
+$wgOut->addHTML("<param name='title' value='".htmlentities($wgTitle->getText())."'/>");
+$wgOut->addHTML("<param name='pageId' value='".$wgTitle->getArticleID()."'/>");
+$wgOut->addHTML("<param name='revId' value='".$wgTitle->getLatestRevID()."'/>");
+$wgOut->addHTML("</applet>");
+//$wgOut->addHTML("<br/>RevID[".$wgTitle->getLatestRevID()."] ID [".$wgTitle->getArticleID()."][".$wgTitle->getText()."][".$wgTitle->getPartialURL()."][".$wgTitle->getDBkey()."][".$wgTitle->getFragment()."]");
+
+return false;
+}
+
+?>
