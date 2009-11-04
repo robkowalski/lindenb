9416524d1312ab1f9e52bfaf1ac6e8292279c27a
plindenbaum <plindenbaum@386288ff-973c-0410-9a7f-93f66fbed5da>
2009-11-04 15:56:42 +0000

    cont

--- trunk/proj/tinytools/src/org/lindenb/tinytools/JenaVelocity.java
+++ trunk/proj/tinytools/src/org/lindenb/tinytools/JenaVelocity.java
@@ -8,6 +8,7 @@ import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
@@ -22,8 +23,19 @@ import org.lindenb.json.JSONParser;
 import org.lindenb.util.AbstractApplication;
 import org.lindenb.util.StringUtils;
 
+import com.hp.hpl.jena.db.DBConnection;
+import com.hp.hpl.jena.db.IDBConnection;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.ModelMaker;
+import com.hp.hpl.jena.rdf.model.RDFReader;
+import com.hp.hpl.jena.sparql.engine.http.QueryExceptionHTTP;
+import com.hp.hpl.jena.sparql.resultset.ResultsFormat;
+import com.hp.hpl.jena.sparql.util.QueryExecUtils;
 
 public class JenaVelocity extends AbstractApplication
 	{
@@ -31,15 +43,46 @@ public class JenaVelocity extends AbstractApplication
 	private Vector<String> templatePath=new Vector<String>();
 	private String primaryTemplate=null;
 	private List<RDFStore> stores= new ArrayList<RDFStore>();
+	private Map<String,Object> mapOfInstances= new HashMap<String,Object>();
 	
+	public class Sparql
+		{
+		public List<Object> select(String serviceURL,String query)
+			{
+			LOG.info("invoke "+query);
+	        QueryExecution qe = QueryExecutionFactory.sparqlService(
+	            		serviceURL, query
+	                    ) ;
+	        // Model m= qe.execDescribe();
+	         List<Object> o=new ArrayList<Object>();
+	         ResultSet row=qe.execSelect();
+	         while(row.hasNext())
+	         	{
+	        	
+	        	o.add(row.next()); 
+	         	}
+	         return o;   
+			 }
+		}
+	
+
 	/** RDFStore, wraps a JenaModel */
 	public static class RDFStore
 		{
 		private Model model;
 		private String name;
-		public long getSize()
+		private IDBConnection conn = null;
+		public Model getModel()
+			{	
+			return this.model;
+			}	
+		public void close()
 			{
-			return this.model.size();
+			if(conn!=null)
+				{
+				try { conn.close(); } catch(Exception err){}
+				}
+			conn=null;
 			}
 		}
 	
@@ -80,6 +123,18 @@ public class JenaVelocity extends AbstractApplication
 			
 			VelocityContext ctx=new VelocityContext();
 			ctx.put("rdfstores", this.stores);
+			ctx.put("sparql", new Sparql());
+			for(RDFStore key:this.stores)
+				{
+				ctx.put(key.name, key.model);
+				}
+			
+			for(String key:this.mapOfInstances.keySet())
+				{
+				ctx.put(key, this.mapOfInstances.get(key));
+				}
+		
+			
 			Template template= engine.getTemplate(this.primaryTemplate);
 			template.merge(ctx, this.out);
 			this.out.flush();
@@ -102,23 +157,27 @@ public class JenaVelocity extends AbstractApplication
 		}
 	
 	
+	
+	private String nonNullString(Map<String,Object> map,String key)
+		{
+		if(!map.containsKey(key)) throw new IllegalArgumentException(map.toString()+" is missing 'name'");
+		Object o=map.get(key);
+		if(o==null || !(o instanceof String)) throw new IllegalArgumentException("'"+key+"' is not a string "+map);
+		return String.class.cast(o);
+		}
+	
+	private String string(Map<String,Object> map,String key)
+		{
+		if(!map.containsKey(key)) return null;
+		Object o=map.get(key);
+		if(o!=null && !(o instanceof String)) throw new IllegalArgumentException("'"+key+"' is not a string "+map);
+		return String.class.cast(o);
+		}
+	
 	private void parseModelDefinition(Map<String,Object> map)
 		{
 		RDFStore store= new RDFStore();
-		if(!map.containsKey("name")) throw new IllegalArgumentException(map.toString()+" is missing 'name'");
-		Object name=map.get("name");
-		if(name==null || !(name instanceof String)) throw new IllegalArgumentException("'name' is not a string "+map);
-		store.name=name.toString();
-		
-		Object base=map.get("base");
-		if(base!=null && !(base instanceof String )) throw new IllegalArgumentException("'base' is not a string "+map);	
-		Object lang=map.get("lang");
-		if(lang!=null && !(lang instanceof String )) throw new IllegalArgumentException("'lang' is not a string "+map);	
-	
-		if(!map.containsKey("url")) throw new IllegalArgumentException(map.toString()+" is missing 'url'");
-		Object url=map.get("url");
-		if(url==null || !(url instanceof String )) throw new IllegalArgumentException("'url' is not a string "+map);	
-		
+		store.name=nonNullString(map,"name");
 		
 		for(RDFStore other:this.stores)
 			{
@@ -127,13 +186,59 @@ public class JenaVelocity extends AbstractApplication
 				throw new IllegalArgumentException("duplicate model name:"+store.name);
 				}
 			}
-		LOG.info("reading RDF Model "+url);
-		store.model= ModelFactory.createDefaultModel();
-		store.model.read(
-			String.class.cast(url),
-			base==null?null:base.toString(),
-			lang==null?null:lang.toString()
-			);
+		
+		if(map.containsKey("url"))
+			{
+			String base = string(map,"base");
+			String lang = string(map,"lang");
+			Object rdfa= map.get("rdfa");
+			String url= nonNullString(map, "url");
+			LOG.info("reading RDF Model "+url);
+			
+			store.model= ModelFactory.createDefaultModel();
+			store.model.read(
+				String.class.cast(url),
+				base,lang
+				);
+			if(map.containsKey("prefix-mapping"))
+				{
+				Map mapping=(Map)map.get("prefix-mapping");
+				for(Object p: mapping.keySet())
+					{
+					store.model.setNsPrefix(p.toString(),mapping.get(p).toString());
+					}
+				}
+			}
+		else if(map.containsKey("jdbc-url"))
+			{
+			String driver=nonNullString(map, "jdbc-driver");
+			try
+				{
+				Class.forName(driver);
+				}
+			catch(Exception err)
+				{
+				throw new IllegalArgumentException("driver not found:"+err.getMessage());
+				}
+			// Create database connection
+			store.conn = new DBConnection(
+					nonNullString(map, "jdbc-url"),
+					nonNullString(map, "user"),
+					nonNullString(map,"password"),
+					nonNullString(map, "type")
+					);
+			ModelMaker maker = ModelFactory.createModelRDBMaker(store.conn) ;
+
+			// create or open the default model
+			store.model = maker.createDefaultModel();
+			}
+		else
+			{
+			throw new IllegalArgumentException("Don't know how to create this model "+map);
+			}
+		
+		
+		
 		this.stores.add(store);
 		}
 	
@@ -179,6 +284,9 @@ public class JenaVelocity extends AbstractApplication
 		super.usage(out);
 		out.println(" -path <path1:path2:path3...> add the path for velocity");
 		out.println(" -t <path> primary template path <REQUIRED>");
+		out.println(" -c <name> <full-class-name> put an new instance of the given class in the velocity context");
+		out.println(" -m <JSON definition> model def");
+		out.println(" -f <JSON definition file> model def");
 		}
 	
 	@Override
@@ -223,6 +331,19 @@ public class JenaVelocity extends AbstractApplication
 				}
 			return optind;
 			}
+		else if(args[optind].equals("-c"))
+			{
+			try {
+				String name= args[++optind];
+				Class<?> clazz= Class.forName(args[++optind]);
+				mapOfInstances.put(name, clazz.newInstance());
+				return optind;
+				} 
+			catch (Exception e)
+				{
+				throw new IllegalArgumentException("Error:"+e.getMessage());
+				}
+			}
 		return super.processArg(args, optind);
 		}
 	/**
@@ -233,7 +354,7 @@ public class JenaVelocity extends AbstractApplication
 		try
 			{
 			JenaVelocity app= new JenaVelocity();
-			app.primaryTemplate="/home/pierre/src/cephb/src/mw4ceph/src/META-INF/runs2wiki.vm";
+			app.processArgs(args);
 			app.run();
 			}
 		catch (Exception e)
