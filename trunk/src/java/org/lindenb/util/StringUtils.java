b3eee2149e2b658b4142662b9649518566310312
plindenbaum <plindenbaum@386288ff-973c-0410-9a7f-93f66fbed5da>
2009-11-10 21:48:05 +0000

    cont

--- trunk/src/java/org/lindenb/util/StringUtils.java
+++ trunk/src/java/org/lindenb/util/StringUtils.java
@@ -47,7 +47,7 @@ public class StringUtils
 	public static String unquote(String s)
 		{
 		if(s==null) return null;
-		if(s.length()>1 && ((s.startsWith("\"") && s.endsWith("\"")) || (s.startsWith("\"") && s.endsWith("\""))))
+		if(s.length()>1 && ((s.startsWith("\'") && s.endsWith("\'")) || (s.startsWith("\"") && s.endsWith("\""))))
 			{
 			return s.substring(1, s.length()-1);
 			}
