2a01ac8e6c2fa39c23133c3c3017eaa61f98a475
plindenbaum <plindenbaum@386288ff-973c-0410-9a7f-93f66fbed5da>
2010-01-08 20:54:42 +0000

    ff2sql

--- trunk/src/java/org/lindenb/util/XMLBeanFactory.java
+++ trunk/src/java/org/lindenb/util/XMLBeanFactory.java
@@ -5,6 +5,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -26,10 +28,13 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
-import com.hp.hpl.jena.reasoner.rulesys.builtins.Regex;
-
+/**
+ * XMLBeanFactory
+ * lightweight implementation of a XML bean factory
+ */
 public class XMLBeanFactory
 	{
+	private static final String NS="http://www.springframework.org/schema/beans";
 	private static Logger LOG= Logger.getLogger("org.lindenb");
 	private Document dom;
 	private HashMap<String, Object> persistence=new HashMap<String, Object>();
@@ -39,20 +44,46 @@ public class XMLBeanFactory
 		this.dom=dom;
 		Element root= this.dom.getDocumentElement();
 		if(root==null) throw new IllegalArgumentException("no root in document");
-		if(root.getLocalName().equals("beans")) 
+		if(!NS.equals(root.getNamespaceURI())) 
+			 throw new IllegalArgumentException("namespace is not "+NS);
+		if(!root.getLocalName().equals("beans")) 
 			 throw new IllegalArgumentException("root is not a <beans/>");
+		
 		//performs eager initialisations
-		for(Element e:XMLUtilities.forEach(root))
-			{
+		
+		
+			for(Element e:XMLUtilities.forEach(root,NS,"bean"))
+				{
+				Attr att= e.getAttributeNode("abstract");
+				if(att!=null && att.getValue().equals("true")) continue;
+				
+				att=root.getAttributeNode("lazy-init");
+				if(att!=null && att.getValue().equals("true")) continue;
+				
+				att= e.getAttributeNode("id");
+				if(att==null) att= e.getAttributeNode("name");
+				if(att==null) continue;
+				String name=att.getValue();
+				Object object=_bean(e);
+				if(!e.getAttribute("singleton").equals("false"))
+					{
+					if(this.persistence.containsKey("name"))
+						{
+						continue;//was initialized by another bean
+						}
+					this.persistence.put(name, object);
+					}
+				}
 			
-			}
 		}
+	
+	
 	
 	private Element findBeanElement(String name)
 		{
 		if(StringUtils.isBlank(name)) throw new IllegalArgumentException("bad name ");
 		
-		for(Element e:XMLUtilities.forEach(this.dom.getDocumentElement(), "bean"))
+		for(Element e:XMLUtilities.forEach(this.dom.getDocumentElement(), NS,"bean"))
 			{
 			Attr att= e.getAttributeNode("id");
 			if(att==null) att= e.getAttributeNode("name");
@@ -60,6 +91,11 @@ public class XMLBeanFactory
 			if(att.getValue().equals(name)) return e;
 			}
 		return null;
+		}
+	
+	public <T> T getBean(String name,Class<T> clazz)
+		{
+		return clazz.cast(getBean(name));
 		}
 	
 	public Object getBean(String name)
@@ -86,52 +122,50 @@ public class XMLBeanFactory
 	
 	private Object _constructor_arg(Element cstorArg)
 		{
-		Object o=null;
-		for(Element e:XMLUtilities.forEach(cstorArg))
-			{
-			o=_eval(cstorArg);
-			}
-		return o;
+		Element e=XMLUtilities.one(cstorArg,NS,null);
+		return _eval(e);
 		}
+	
 	private Object _eval(Element any)
 		{
-		if(any.getNodeName().equals("ref"))
+		String local=any.getLocalName();
+		if(local.equals("ref"))
 			{
 			return _ref(any);
 			}
-		else if(any.getNodeName().equals("value"))
+		else if(local.equals("value"))
 			{
 			return _value(any);
 			}
-		else if(any.getNodeName().equals("null"))
+		else if(local.equals("null"))
 			{
 			return _null(any);
 			}
-		else if(any.getNodeName().equals("props"))
+		else if(local.equals("props"))
 			{
 			return _props(any);
 			}
-		else if(any.getNodeName().equals("list"))
+		else if(local.equals("list"))
 			{
 			return _list(any);
 			}
-		else if(any.getNodeName().equals("map"))
+		else if(local.equals("map"))
 			{
 			return _map(any);
 			}
-		else if(any.getNodeName().equals("set"))
+		else if(local.equals("set"))
 			{
 			return _set(any);
 			}
-		else if(any.getNodeName().equals("key"))
+		else if(local.equals("key"))
 			{
 			return _key(any);
 			}
-		else if(any.getNodeName().equals("bean"))
+		else if(local.equals("bean"))
 			{
 			return _bean(any);
 			}
-		else if(any.getNodeName().equals("idref"))
+		else if(local.equals("idref"))
 			{
 			return _idref(any);
 			}
@@ -193,7 +227,7 @@ public class XMLBeanFactory
 	private Properties _props(Element root)
 		{
 		Properties ppties=new Properties();
-		for(Element prop:XMLUtilities.forEach(root,"prop"))
+		for(Element prop:XMLUtilities.forEach(root,NS,"prop"))
 			{
 			Attr att=prop.getAttributeNode("key");
 			if(att==null) throw new IllegalArgumentException("@key missing in prop");
@@ -210,7 +244,7 @@ public class XMLBeanFactory
 	private List<?> _list(Element root)
 		{
 		List<Object> L=new ArrayList<Object>();
-		for(Element li:XMLUtilities.forEach(root))
+		for(Element li:XMLUtilities.forEach(root,NS,null))
 			{
 			L.add(_eval(li));
 			}
@@ -220,78 +254,141 @@ public class XMLBeanFactory
 	private Object _bean(Element e)
 		{
 		Attr classAtt= e.getAttributeNode("class");
-		Attr parentAtt= e.getAttributeNode("parent");
+		
 		Attr abstractAtt= e.getAttributeNode("abstract");
-		Attr autoWireAtt= e.getAttributeNode("autowire");
+		//Attr autoWireAtt= e.getAttributeNode("autowire");
 		
 		
 		Object object=null;
 		if(classAtt==null)
 			{
-			//TODO
+			throw new IllegalArgumentException("missing @class in bean");
 			}
-		else
+		if(abstractAtt!=null && abstractAtt.getValue().equals("true"))
 			{
-			String className= classAtt.getValue();
-			if(StringUtils.isBlank(className)) throw new IllegalArgumentException("Bad @class in bean");
-			try
+			throw new IllegalArgumentException("bean is abstract");
+			}
+		String className= classAtt.getValue();
+		if(StringUtils.isBlank(className)) throw new IllegalArgumentException("Bad @class in bean");
+		try
+			{
+			Class<?> clazz= Class.forName(className);
+			List<Object> arguments= new ArrayList<Object>();
+			
+			for(Element cstor:XMLUtilities.forEach(e,NS,"constructor-arg"))
 				{
-				Class<?> clazz= Class.forName(className);
-				List<Object> arguments= new ArrayList<Object>();
-				
-				for(Element cstor:XMLUtilities.forEach(e,"constructor-arg"))
+				if(XMLUtilities.count(cstor)==0)
 					{
-					arguments.add(_constructor_arg(cstor));
+					throw new NotFoundException("Empty constructor-arg");
 					}
-				
-				if(arguments.isEmpty())
+				arguments.add(_constructor_arg(cstor));
+				}
+			
+			if(arguments.isEmpty())
+				{
+				object=clazz.newInstance();
+				}
+			else
+				{
+				for(Constructor<?> ctor:clazz.getConstructors())
 					{
-					object=clazz.newInstance();
-					}
-				else
-					{
-					for(Constructor<?> ctor:clazz.getConstructors())
+					Class<?> paramType[]=ctor.getParameterTypes();
+					if(paramType.length!=arguments.size()) continue;
+					int i=0;
+					for(i=0;i< arguments.size();++i)
 						{
-						Class<?> paramType[]=ctor.getParameterTypes();
-						if(paramType.length!=arguments.size()) continue;
+						Object param=arguments.get(i);
+						if(param==null && paramType[i].isPrimitive()) break;
+						if(param!=null && !paramType[i].isInstance(param)) break;
 						}
-					if(object==null) throw new RuntimeException("Cannot initialize object ctor");
+					if(i!=arguments.size()) continue;
+					object=ctor.newInstance(arguments.toArray());
+					break;
 					}
+				if(object==null) throw new RuntimeException("Cannot initialize object ctor");
 				}
-			catch (Exception e2)
-				{
-				throw new RuntimeException(e2);
-				}
+			}
+		catch (Exception e2)
+			{
+			throw new RuntimeException(e2);
 			}
 		
-		for(Element prop:XMLUtilities.forEach(e,"property"))
-			{
-			applyProperty(prop,object);
-			}
+		_abstractBean(e,object);
+		
 		return object;
 		}
 	
-	private void applyProperty(Element root,Object object)
+	private void _abstractBean(Element root,Object object)
+		{
+		Attr parentAtt= root.getAttributeNode("parent");
+		if(parentAtt!=null)
+			{
+			for(String parentName:parentAtt.getValue().split("[, ]+"))
+				{
+				Element p= findBeanElement(parentName);
+				if(p==null) throw new NotFoundException("cannot find parent bean "+parentName);
+				_abstractBean(p,object);
+				}
+			}
+		for(Element prop:XMLUtilities.forEach(root,NS,"property"))
+			{
+			applyProperty(prop,object);
+			}
+		}
+	
+	private void applyProperty(Element root,Object bean)
 		{
 		Attr nameAtt=root.getAttributeNode("name");
 		if(nameAtt==null) throw new RuntimeException("@name misssing in property");
 		String name=nameAtt.getValue();
 		if(!name.matches("[a-z_][a-zA-Z0-9_]*")) throw new RuntimeException("bad name :"+name);
-		
+		Object value=null;
 		if(!root.hasChildNodes())
 			{
 			Attr valueAttr=root.getAttributeNode("value");
 			if(valueAttr==null) throw new RuntimeException("@value misssing in property");
-			
+			//TODO
+			}
+		else
+			{
+			value=_eval(XMLUtilities.one(root));
 			}
 		
+		String setter="set"+(name.length()==1?
+				name.toUpperCase():
+				name.substring(0,1).toUpperCase()+name.substring(1)
+				);
+		try
+			{
+			Method method=null;
+			for(Method m: bean.getClass().getMethods())
+				{
+				if(!m.getName().equals(setter)) continue;
+				int modifiers=m.getModifiers();
+				if(Modifier.isStatic(modifiers)) continue;
+				if(!Modifier.isPublic(modifiers)) continue;
+				Class<?> types[]=m.getParameterTypes();
+				if(types.length!=1) continue;
+				Class<?> type=types[0];
+				if(value==null && type.isPrimitive()) continue;
+				if(value!=null && !type.isInstance(value)) continue;
+				if(method!=null) throw new RuntimeException("ambigous method "+setter+" to "+bean.getClass());
+				method=m;
+				}
+			if(method==null) throw new RuntimeException("Cannot find method "+setter+" to "+bean.getClass());
+			method.invoke(bean, value);
+			}
+		catch (Exception e)
+			{
+			throw new RuntimeException(e);
+			}
 		}
 	
 	
 	private Map<?, ?> _map(Element root)
 		{
 		Map<Object,Object> M=new HashMap<Object,Object>();
-		for(Element entry:XMLUtilities.forEach(root,"entry"))
+		for(Element entry:XMLUtilities.forEach(root,NS,"entry"))
 			{
 			Pair<Object,Object> p=_mapEntry(entry);
 			M.put(p.first(), p.second());
@@ -302,7 +399,7 @@ public class XMLBeanFactory
 	private Set<?> _set(Element root)
 		{
 		Set<Object> S=new HashSet<Object>();
-		for(Element li:XMLUtilities.forEach(root))
+		for(Element li:XMLUtilities.forEach(root,NS,null))
 			{
 			S.add(_eval(li));
 			}
@@ -311,13 +408,7 @@ public class XMLBeanFactory
 	
 	private Object _key(Element root)
 		{
-		Element value=null;
-		for(Element e:XMLUtilities.forEach(root))
-			{
-			if(value!=null) throw new IllegalArgumentException("illegal key");
-			value=e;
-			}
-		if(value==null) throw new IllegalArgumentException("element missing under key");
+		Element value=XMLUtilities.one(root,NS,null);
 		return _eval(value);
 		}
 	
@@ -346,7 +437,7 @@ public class XMLBeanFactory
 			}
 		Element eK=null;
 		Element eV=null;
-		for(Element e:XMLUtilities.forEach(root))
+		for(Element e:XMLUtilities.forEach(root,NS,null))
 			{
 			if(e.getNodeName().equals("key"))
 				{
@@ -367,6 +458,16 @@ public class XMLBeanFactory
 		Object value= _eval(eV);
 		return new Pair<Object,Object>(key,value);
 		}
+	
+	/*private Object _invoke(Element root)
+		{
+		Attr beanAtt=root.getAttributeNode("bean");
+		if(beanAtt==null)  throw new IllegalArgumentException("@bean missing under "+root.getNodeName());
+		Attr nameAtt=root.getAttributeNode("name");
+		if(nameAtt==null)  throw new IllegalArgumentException("@name missing under "+root.getNodeName());
+		Object instance=getBean(beanAtt.getValue());
+		
+		}*/
 	
 	public static XMLBeanFactory loadInstance(File file)
 		throws IOException
