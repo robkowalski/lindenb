12d24dd5417f4af4b3d82acc9117fcf9b801188a
plindenbaum <plindenbaum@386288ff-973c-0410-9a7f-93f66fbed5da>
2010-07-06 10:01:48 +0000

    cont

--- src/java/org/lindenb/sql/AbstractSqlList.java
+++ src/java/org/lindenb/sql/AbstractSqlList.java
@@ -6,9 +6,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.AbstractList;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
-import javax.sql.DataSource;
+import java.util.Map;
+import java.util.RandomAccess;
+import java.util.TreeMap;
 
 /**
  * Abstract lightweight List for SQL
@@ -18,27 +20,83 @@ import javax.sql.DataSource;
  */
 public abstract class AbstractSqlList<T>
 	extends AbstractList<T>
+	implements RandomAccess
 	{
 	private int _size=-1;
 	private int _index=-1;
 	private List<T> buffer=null;
-	public abstract DataSource getDataSource();
-	public abstract ObjectSQLFactory<T> getObjectSQLFactory();
 	
-	protected Connection getConnection() throws SQLException
+	protected abstract Connection getConnection()  throws SQLException;
+	protected abstract void releaseConnection(Connection con) throws SQLException;
+	protected abstract ObjectSQLFactory<T> getObjectSQLFactory();
+	
+	protected AbstractSqlList()
 		{
-		return getDataSource().getConnection();
 		}
+	
 	protected int getBufferSize()
 		{
 		return 1000;
 		}
 	protected abstract String getSelectCountQuery();
-	protected abstract String getSelectQuery(int start,int count);
+	protected abstract String getSelectQuery();
+	
+	@SuppressWarnings("unchecked")
+	protected Map<Integer,Object> getParams()
+		{
+		return Collections.EMPTY_MAP;
+		}
+	
+	protected String getSelectQuery(int start,int count)
+		{
+		return getSelectQuery()+" limit "+start+","+count;
+		}
+	
+	protected void fillPreparedStatement(PreparedStatement pstmt) throws SQLException
+		{
+		for(Integer i0: this.getParams().keySet())
+			{
+			int i=i0+1;
+			Object o= this.getParams().get(i0);
+			if(o.getClass()==String.class || o instanceof CharSequence || o.getClass().isEnum())
+				{
+				pstmt.setString(i, o.toString());
+				}
+			else if(o.getClass()==Long.class)
+				{
+				pstmt.setLong(i, Long.class.cast(o));
+				}
+			else if(o.getClass()==Integer.class)
+				{
+				pstmt.setInt(i, Integer.class.cast(o));
+				}
+			else if(o.getClass()==Short.class)
+				{
+				pstmt.setShort(i, Short.class.cast(o));
+				}
+			else if(o.getClass()==Byte.class)
+				{
+				pstmt.setByte(i, Byte.class.cast(o));
+				}
+			else if(o.getClass()==Float.class)
+				{
+				pstmt.setFloat(i, Float.class.cast(o));
+				}
+			else if(o.getClass()==Double.class)
+				{
+				pstmt.setDouble(i, Double.class.cast(o));
+				}
+			else
+				{
+				pstmt.setObject(i, o);
+				}
+			}
+		}
 	
 	protected void load(int position)
 		{
-		if( position>= this._index &&
+		if( this.buffer!=null &&
+			position>= this._index &&
 			position< this._index+this.buffer.size()) return;
 		Connection con=null;
 		PreparedStatement pstmt=null;
@@ -48,6 +106,7 @@ public abstract class AbstractSqlList<T>
 			{
 			con= getConnection();
 			pstmt=con.prepareStatement(getSelectQuery(position,getBufferSize()));
+			fillPreparedStatement(pstmt);
 			row=pstmt.executeQuery();
 			
 			if(this.buffer==null)
@@ -61,7 +120,8 @@ public abstract class AbstractSqlList<T>
 			
 			while(row.next())
 				{
-				buffer.add(getObjectSQLFactory().newInstance(row));
+				T object=getObjectSQLFactory().newInstance(row);
+				buffer.add(object);
 				}
 			this._index=position;
 			}
@@ -85,7 +145,7 @@ public abstract class AbstractSqlList<T>
 				}
 			if(con!=null)
 				{
-				try { con.close(); }
+				try {releaseConnection(con); }
 				catch(SQLException err) {}
 				}
 			
@@ -101,6 +161,7 @@ public abstract class AbstractSqlList<T>
 			{
 			con= getConnection();
 			pstmt=con.prepareStatement(getSelectCountQuery());
+			fillPreparedStatement(pstmt);
 			row=pstmt.executeQuery();
 			while(row.next())
 				{
@@ -126,7 +187,7 @@ public abstract class AbstractSqlList<T>
 				}
 			if(con!=null)
 					{
-					try { con.close(); }
+					try { releaseConnection(con); }
 					catch(SQLException err) {}
 					}
 			
@@ -151,6 +212,10 @@ public abstract class AbstractSqlList<T>
 		return _size;
 		}
 	
+	public final int getSize()
+		{	
+		return this.size();
+		}
 	
-
+	
 	}
